a99a57548a1fe964d13d9795f3bd4b9cff58a5a7
andre <andre>
2004-12-20 18:24:43 +0000

    Install sievectl.

--- install.rb
+++ install.rb
@@ -1,14 +1,30 @@
-# $Id: install.rb,v 1.1 2004/12/20 17:49:51 andre Exp $
+# $Id: install.rb,v 1.2 2004/12/20 18:24:43 andre Exp $
 
 require 'fileutils'
 require 'rbconfig'
 
 lib = 'lib/managesieve.rb'
+bin = 'bin/sievectl'
 libdir = Config::CONFIG['sitelibdir']
+bindir = Config::CONFIG['bindir']
 
 begin
   puts "#{lib} -> #{libdir}"
   FileUtils::cp(lib, libdir)
 rescue => e
-  puts "Cannot install: #{e}"
+  puts "Cannot install #{lib}: #{e}"
+end
+
+begin
+  puts "#{bin} -> #{bindir}"
+  FileUtils::cp(bin, bindir)
+  File::chmod(0755, "#{bindir}/sievectl")
+rescue => e
+  puts "Cannot install #{bin}: #{e}"
+end
+
+begin
+  system("rdoc --ri-site #{lib}")
+rescue => e
+  puts "Cannot generate RDoc: #{e}"
 end
